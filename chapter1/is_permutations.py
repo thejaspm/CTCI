c073235970f851638815f35d1b4bf85ec254b643
Thejas <thejas.p.murthy@gmail.com>
2019-05-29 12:55:44 +0000

    type hinting

--- chapter1/is_permutations.py
+++ chapter1/is_permutations.py
@@ -1,3 +1,3 @@
-def solution(x:str, y:str):
+def solution(x:str, y:str) -> bool:
   from functools import reduce
   return reduce(lambda x,y: x&y , [True if a == b else False for a,b in zip(sorted(x),sorted(y))])
